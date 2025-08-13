beb7fee8bd0c5367ab3234c032272b073eefbc8c
Thanish C <thanish.kc@gmail.com>
2025-08-13 17:44:52 +0530

    Initialize models, classifiers, and audio for drowsiness detection

--- drowsinessdetection.py
+++ drowsinessdetection.py
@@ -1,7 +1,26 @@
 # Import Section
 import cv2
 import os
-from keras.module import load_model
+from keras.models import load_model
 import numpy as np
 from pygame import mixer
 import time
+
+mixer.init()
+sound = mixer.Sound('alarm.wav')
+
+face = cv2.CascadeClassifier('haar cascade files/haarcascade_frontalface_alt.xml')
+leye = cv2.CascadeClassifier('haar cascade files/haarcascade_lefteye_2splits.xml')
+reye = cv2.CascadeClassifier('haar cascade files/haarcascade_righteye_2splits.xml')
+
+lbl = ['Close', 'Open']
+
+model = load_model('models/cnncat2.h5')
+path = os.getcwd()
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+count = 0
+score = 0
+thicc = 2
+rpred = [99]
+lpred = [99]
