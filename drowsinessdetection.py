4ab6d66bc28fca1b7a9fa71f95b1f7afc841effc
Thanish C <thanish.kc@gmail.com>
2025-08-13 21:42:24 +0530

    Implement real-time eye state detection and prediction loop

--- drowsinessdetection.py
+++ drowsinessdetection.py
@@ -24,3 +24,54 @@ score = 0
 thicc = 2
 rpred = [99]
 lpred = [99]
+
+while True:
+    ret, frame = cap.read()
+    height, width = frame.shape[:2]
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = face.detectMultiScale(gray, minNeighbors=5, scaleFactor=1.1, minSize=(25, 25))
+    left_eye = leye.detectMultiScale(gray)
+    right_eye = reye.detectMultiScale(gray)
+
+    cv2.rectangle(frame, (0, height - 50), (200, height), (0, 0, 0), thickness=cv2.FILLED)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 100, 100), 1)
+
+    for (x, y, w, h) in right_eye:
+        r_eye = frame[y:y + h, x:x + w]
+        count += 1
+        r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2GRAY)
+        r_eye = cv2.resize(r_eye, (24, 24))
+        r_eye = r_eye / 255
+        r_eye = r_eye.reshape(24, 24, -1)
+        r_eye = np.expand_dims(r_eye, axis=0)
+        #rpred = model.predict_classes(r_eye)
+        #rpred = model.predict(r_eye)
+        # rpred = np.round(rpred).astype(int)
+        rpred = np.argmax(model.predict(r_eye), axis=-1)
+        if rpred[0] == 1:
+            lbl = 'Open'
+        if rpred[0] == 0:
+            lbl = 'Closed'
+        break
+
+    for (x, y, w, h) in left_eye:
+        l_eye = frame[y:y + h, x:x + w]
+        count += 1
+        l_eye = cv2.cvtColor(l_eye, cv2.COLOR_BGR2GRAY)
+        l_eye = cv2.resize(l_eye, (24, 24))
+        l_eye = l_eye / 255
+        l_eye = l_eye.reshape(24, 24, -1)
+        l_eye = np.expand_dims(l_eye, axis=0)
+        #lpred = model.predict_classes(l_eye)
+        #lpred = model.predict(l_eye)
+        # lpred = np.round(lpred).astype(int)
+        lpred = np.argmax(model.predict(l_eye), axis=-1)
+        if lpred[0] == 1:
+            lbl = 'Open'
+        if lpred[0] == 0:
+            lbl = 'Closed'
+        break
